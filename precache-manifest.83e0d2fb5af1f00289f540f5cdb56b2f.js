0cc84a0231e44c281a8c43199f2153b7daf8050c
Thisseas X <thisseasx@yahoo.com>
2020-01-26 18:12:58 +0200

    Updates

---
+++ precache-manifest.83e0d2fb5af1f00289f540f5cdb56b2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4ce75f62c8ad27c7cc78e1c6ec27a40",
+    "url": "/react-memo-test/index.html"
+  },
+  {
+    "revision": "f8f9119d5286946d2a75",
+    "url": "/react-memo-test/static/js/2.fad3d9ae.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-memo-test/static/js/2.fad3d9ae.chunk.js.LICENSE"
+  },
+  {
+    "revision": "38cc28edf0b21c2082bf",
+    "url": "/react-memo-test/static/js/main.279abd68.chunk.js"
+  },
+  {
+    "revision": "25c07005b89ecbb68bfa",
+    "url": "/react-memo-test/static/js/runtime-main.1639b3e4.js"
+  }
+]);
